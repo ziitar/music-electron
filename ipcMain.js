ea9b8f156ba65c0078b9c2e11ce215c9ba2d1baa
ZIITAR\uziit <ziitar@163.com>
2023-05-08 16:36:28 +0800

    update

--- ipcMain.js
+++ ipcMain.js
@@ -1,12 +1,11 @@
 const { ipcMain } = require("electron");
-const { getID3, setID3, setID3WithNodeID3 } = require("./util.js");
+const { getID3, setID3 } = require("./util.js");
 
 function registerIpcMain() {
   ipcMain.handle("read-ID3", async (event, root, file) => {
     return getID3(root, file);
   });
   ipcMain.handle("set-ID3", async (event, root, file, tags) => {
-    console.log(root, file, JSON.stringify(tags));
     return setID3(root, file, tags);
   });
 }
